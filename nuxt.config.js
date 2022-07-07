b538f4894778778630f726a59519a856e03fc830
Saša Štrbački <sasa.strbacki@marbleit.rs>
2022-07-07 13:42:30 +0200

    Tailwind configured

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'cats-app',
+    title: 'Welcome to the Cat App',
     htmlAttrs: {
       lang: 'en',
     },
